af37ea17e18217ab11c528b3b3231a04cbc1aed6
Fikri-Aldi-Nugraha <fikrialdinugraha@gmail.com>
2018-07-14 00:16:03 +0700

    TB-Android

---
+++ Parkir/app/src/main/java/com/example/hp/parkir/Login.java
@@ -0,0 +1,28 @@
+package com.example.hp.parkir;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    private Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
